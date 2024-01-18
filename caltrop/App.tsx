9b38a1a414aff3a640be999f396fe6800a2b8900
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2024-01-18 10:23:49 -0800

    WIP

--- caltrop/App.tsx
+++ caltrop/App.tsx
@@ -3,73 +3,48 @@ import { StyleSheet, View } from 'react-native';
 import { YearGrid } from './components/YearGrid';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, BottomNavigation, Icon } from 'react-native-paper';
+import { Text, BottomNavigation, Icon, PaperProvider } from 'react-native-paper';
 import { CommonActions, NavigationContainer } from '@react-navigation/native';
 import { useMemo } from 'react';
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 
-const Tab = createBottomTabNavigator();
+import * as React from 'react';
 
-enum TabNames {
-  Workout = "Workout",
-  Food = "Food",
-}
+const WorkoutRoute = () => <Text>Workout</Text>;
 
-const TAB_ICONS = {
-  [TabNames.Workout]: 'dumbbell',
-  [TabNames.Food]: 'food-apple',
-}
+const FoodRoute = () => <Text>Food</Text>;
 
-// TODO: remove any
-const YearTab = ({ route, navigation }: any) => {
-  return useMemo(() => (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <YearGrid
-        title={new Date().getFullYear().toString() + " - " + route.name}
-        onStateChange={(month, date, state) => console.log(month, date, state)}
-      />
-    </SafeAreaView >
-  ), [route.name]);
-}
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
+
+const MyComponent = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'workout', title: 'Workout', focusedIcon: 'dumbbell' },
+    { key: 'food', title: 'Food', focusedIcon: 'food-apple' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    workout: WorkoutRoute,
+    food: FoodRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{ headerShown: false, lazy: false }}
-        tabBar={({ navigation, state, descriptors, insets }) => (
-          <BottomNavigation.Bar
-            navigationState={state}
-            safeAreaInsets={insets}
-            onTabPress={({ route, preventDefault }) => {
-              const event = navigation.emit({
-                type: 'tabPress',
-                target: route.key,
-                canPreventDefault: true,
-              });
-
-              if (event.defaultPrevented) {
-                preventDefault();
-              } else {
-                navigation.dispatch({
-                  ...CommonActions.navigate(route.name, route.params),
-                  target: state.key,
-                });
-              }
-            }}
-            renderIcon={({ route, focused, color }) => {
-              return (<Icon source={TAB_ICONS[route.name as TabNames]} size={20} />);
-            }}
-            getLabelText={({ route }) => {
-              return route.name;
-            }}
-          />
-        )}
-      >
-        <Tab.Screen name={TabNames.Workout} component={YearTab}></Tab.Screen>
-        <Tab.Screen name={TabNames.Food} component={YearTab}></Tab.Screen>
-      </Tab.Navigator>
+      <PaperProvider>
+        <MyComponent />
+      </PaperProvider>
     </NavigationContainer>
   );
 }
