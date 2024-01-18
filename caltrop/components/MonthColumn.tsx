2074de6cc1c8116464e7d398e53101a24f057943
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2024-01-18 00:04:46 -0800

    Perf

--- caltrop/components/MonthColumn.tsx
+++ caltrop/components/MonthColumn.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { DayButton } from './DayButton';
-import { FlatList, View } from 'react-native';
+import { FlatList, ScrollView, View } from 'react-native';
 import { StyleSheet } from "react-native";
 import { Text } from 'react-native-paper';
 
@@ -16,16 +16,17 @@ const MonthColumn = (props: MonthProps) => {
     return (
         <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
             <Text variant='titleLarge'>{props.title}</Text>
-            <FlatList
-                scrollEnabled={false}
-                data={Array.from({ length: props.days }, (x, i) => i + 1)}
-                renderItem={({ item }) => {
-                    return (<DayButton
-                        onStateChange={(state) => props?.onStateChange?.(item, state)}>
-                        {item}
-                    </DayButton>);
-                }}
-            />
+            <ScrollView scrollEnabled={false}>
+                {Array.from({ length: props.days }, (x, i) => i + 1).map((item => {
+                    return (
+                        <DayButton
+                            key={item}
+                            onStateChange={(state) => props?.onStateChange?.(item, state)}>
+                            {item}
+                        </DayButton>
+                    );
+                }))}
+            </ScrollView>
         </View>
     );
 }
