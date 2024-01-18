96dd94a67b24207fb0f6be5616e95e40110e6aa0
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2024-01-18 09:33:57 -0800

    Bottom Tabs 2

--- caltrop/App.tsx
+++ caltrop/App.tsx
@@ -8,51 +8,25 @@ import { NavigationContainer } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 
-
-function HomeScreen() {
+function YearTab({ route, navigation }) {
   return (
-    <YearGrid
-      title={new Date().getFullYear().toString() + " - Workout"}
-      onStateChange={(month, date, state) => console.log(month, date, state)}
-    />
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <YearGrid
-      title={new Date().getFullYear().toString() + " - Food"}
-      onStateChange={(month, date, state) => console.log(month, date, state)}
-    />
-  );
-}
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
-
-export default function YearTab() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <YearGrid
+        title={new Date().getFullYear().toString() + " - " + route.params.title}
+        onStateChange={(month, date, state) => console.log(month, date, state)}
+      />
+    </SafeAreaView >
   );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <StatusBar style="auto" />
-        <YearGrid
-          onStateChange={(month, date, state) => console.log(month, date, state)}
-        />
-      </SafeAreaView >
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Workout" component={YearTab} initialParams={{ title: "Workout" }}></Tab.Screen>
+        <Tab.Screen name="Food" component={YearTab} initialParams={{ title: "Food" }}></Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
