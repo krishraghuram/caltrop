f55e2a1504c5d76b8d3b71d49b2e1ffc8f6323f2
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2024-01-18 09:16:35 -0800

    Revert "Tried BottomNavigation, Perf is poor"
    This reverts commit 019b563820fbec87c5bfd16fe78025612861fb10.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,8 @@
 TODO
 
-* Show Year ✓
 * Persist data
-* Allow showing a "title"
 * Allow recording multiple "tabs"
+* Allow showing a "title"
 * Assumes all years are non-leap years
 * Year is not recorded, only month and day and state are recorded
 * Does not allow "clearing"... well, one could clear the entire app's data in android settings...
--- caltrop/App.tsx
+++ caltrop/App.tsx
@@ -1,61 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { YearGrid } from './components/YearGrid';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { BottomNavigation, PaperProvider } from 'react-native-paper';
-import { useCallback, useMemo, useState } from 'react';
-import { Text } from 'react-native-paper';
 
-const WorkoutRoute = () => <Text>Workout</Text>;
-
-const FoodRoute = () => <Text>Food</Text>;
-
-const YearRoute = (props: any) => {
+export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <YearGrid
-        title={props.title}
         onStateChange={(month, date, state) => console.log(month, date, state)}
       />
     </SafeAreaView >
   );
-}
-
-export default function App() {
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'workout', title: 'Workout', focusedIcon: 'dumbbell' },
-    { key: 'food', title: 'Food', focusedIcon: 'food-apple' },
-  ]);
-
-  // const renderScene = BottomNavigation.SceneMap({
-  //   workout: () => <YearRoute title={new Date().getFullYear().toString() + " - Workout"}></YearRoute>,
-  //   food: () => <YearRoute title={new Date().getFullYear().toString() + " - Food"}></YearRoute>,
-  // });
-  const renderScene = BottomNavigation.SceneMap({
-    workout: () => <Text>{new Date().getFullYear().toString() + " - Workout"}</Text>,
-    food: () => <Text>{new Date().getFullYear().toString() + " - Food"}</Text>,
-  });
-
-  return (
-    <PaperProvider>
-      <BottomNavigation
-        navigationState={{ index, routes }}
-        onIndexChange={setIndex}
-        renderScene={renderScene}
-      />
-    </PaperProvider>
-  );
-
-  // return (
-  //   <SafeAreaView style={styles.container}>
-  //     <StatusBar style="auto" />
-  //     <YearGrid
-  //       onStateChange={(month, date, state) => console.log(month, date, state)}
-  //     />
-  //   </SafeAreaView >
-  // );
 }
 
 const styles = StyleSheet.create({
